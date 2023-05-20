7c857e42e3f9f045267eeb8043a6b8538aec066a
Alexandra Kurganova <axndkgn@icloud.com>
2023-05-20 15:55:30 +0300

    Initial Commit

---
+++ hwSuffixArrayAsync/hwSuffixArrayAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwSuffixArrayAsyncApp.swift
+//  hwSuffixArrayAsync
+//
+//  Created by Alexandra Kurganova on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct hwSuffixArrayAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
